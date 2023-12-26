70f448be09bc9e2fbedc862b5857fea05a57f742
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-12-26 14:39:47 -0700

    Initial commit for threejs testing projects

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+import * as THREE from 'three';
+
+function App() {
+
+  return (
+    <>
+      <p>Hello world</p>
+    </>
+  )
+}
+
+export default App
