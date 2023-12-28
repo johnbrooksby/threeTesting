6dfe4f215fcc59359ccd99e17e5ce675007c4047
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-12-27 20:35:48 -0700

    Start built out and rendering correctly

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,27 @@ import React, { useState } from 'react'
 import './App.css'
 import * as THREE from 'three';
 
+const canvas = document.querySelector("canvas.webgl")
+
 function App() {
+
+  const scene = new THREE.Scene()
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: "red" })
+  const mesh = new THREE.Mesh(geometry, material)
+
+  scene.add(mesh)
+
+  const sizes = {
+    width: 800,
+    height: 600
+  }
+
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+  scene.add(camera)
+
+  const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+  renderer.setSize(sizes.width, sizes.height)
 
   return (
     <>
