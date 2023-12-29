d9dc3ac496cadf007ca05f3f424dfa8ab990b0cd
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-12-28 22:00:58 -0700

    A few method functions added for reference

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "red" });
 const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1)
+mesh.position.normalize()
 
 scene.add(mesh);
 
@@ -15,7 +20,7 @@ const sizes = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.z = 1;
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
